af43fb8310fa4d2a11288f3bb8e913a9565604ff
Zhang Hao <zh975525512@163.com>
2022-03-04 16:56:39 +0800

    第三次作业工厂模式，随机数产生仍有问题

--- Homework03/Homework03/Program.cs
+++ Homework03/Homework03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Homework03
+﻿using System;
+
+namespace Homework03
 {
     public interface IGetSize
     {
@@ -10,7 +12,13 @@
         bool IsShape();
     }
 
-    public class Rectangle : IGetSize, IIsShape
+    public abstract class Shape : IGetSize, IIsShape
+    {
+        public abstract double GetSize();
+        public abstract bool IsShape();
+    }
+
+    public class Rectangle :Shape
     {
         private double _height;
         private double _width;
@@ -33,12 +41,12 @@
             this._width = width;
         }
 
-        public double GetSize()
+        public override double GetSize()
         {
             return _height * _width;
         }
 
-        public bool IsShape()
+        public override bool IsShape()
         {
             if (_height <= 0 || _width <= 0)
             {
@@ -65,7 +73,7 @@
         
     }
 
-    public class Triangle : IGetSize, IIsShape
+    public class Triangle: Shape
     {
         private double _side1;
         private double _side2;
@@ -96,7 +104,7 @@
             _side3 = side3;
         }
 
-        public bool IsShape()
+        public override bool IsShape()
         {
             if (_side1 <= 0 || _side2 <= 0 || _side3 <= 0 || _side1 + _side2 <= _side3 || _side1 + _side3 <= _side2 || _side2 + _side3 <= _side1)
             {
@@ -106,18 +114,77 @@
             return true;
         }
 
-        public double GetSize()
+        public override double GetSize()
         {
             double halfLength = (_side1 + _side2 + _side3) / 2;
-            return System.Math.Sqrt(halfLength * (halfLength - _side1) * (halfLength - _side2) * (halfLength - _side3));
+            return Math.Sqrt(halfLength * (halfLength - _side1) * (halfLength - _side2) * (halfLength - _side3));
         }
         
+    }
+    
+    // 工厂设计模式
+    public class ShapeFactory
+    {
+        public static Shape GetShapeFactory(params double[] sides)
+        {
+            switch (sides.Length)
+            {
+                case 1: 
+                    return new Square(sides[0]);
+                case 2: 
+                    return new Rectangle(sides[0], sides[1]);
+                case 3: 
+                    return new Triangle(sides[0], sides[1], sides[2]);
+                default:
+                    throw new ArgumentException("参数错误");
+            }
+        }
     }
     
     internal class Program
     {
         public static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                Random rand = new Random();
+                switch (rand.Next(0, 3))
+                {
+                    case 0:
+                        Shape s = ShapeFactory.GetShapeFactory(rand.NextDouble());
+                        if (s.IsShape())
+                        {
+                            Console.WriteLine("第" + i + "个图形为正方形，面积为" + s.GetSize());
+                        }
+                        else
+                        {
+                            Console.WriteLine("第" + i + "图形不为正方形");
+                        }
+                        break;
+                    case 1:
+                        Shape t = ShapeFactory.GetShapeFactory(rand.NextDouble(), rand.NextDouble());
+                        if (t.IsShape())
+                        {
+                            Console.WriteLine("第" + i + "图形为长方形，面积为" + t.GetSize());
+                        }
+                        else
+                        {
+                            Console.WriteLine("第" + i + "图形不为正方形");
+                        }
+                        break;
+                    case 2:
+                        Shape q = ShapeFactory.GetShapeFactory(rand.NextDouble(), rand.NextDouble(), rand.NextDouble());
+                        if (q.IsShape())
+                        {
+                            Console.WriteLine("第" + i + "图形为三角形，面积为" + q.GetSize());
+                        }
+                        else
+                        {
+                            Console.WriteLine("第" + i + "图形不为正方形");
+                        }
+                        break;
+                }
+            }
         }
     }
 }
